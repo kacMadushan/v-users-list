7374385ce14a9881c8b704ca3a951281fc29f66d
kacMadushan <markuptutorials@gmail.com>
2026-06-05 22:43:03 +0530

    Add initial users state

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,7 +1,31 @@
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', {
-    state: () => ({}),
+    state: () => ({
+        users: [
+            {
+                id: "1",
+                name: "",
+                email: "",
+                mobile: "",
+                group: ""
+            },
+            {
+                id: "2",
+                name: "",
+                email: "",
+                mobile: "",
+                group: ""
+            },
+            {
+                id: "3",
+                name: "",
+                email: "",
+                mobile: "",
+                group: ""
+            }
+        ]
+    }),
     getters: {},
     actions: {}
 })
