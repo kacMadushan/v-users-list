e4ee9f6b40b721e7e11c37e4eb7b292615f6d6a6
kacMadushan <markuptutorials@gmail.com>
2026-06-05 21:33:14 +0530

    Add views in main router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Users from "@/views/Users/Users.vue"
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        {
+            path: '/',
+            name: 'users',
+            component: Users
+        },
+        {
+            path: '/profile/:id',
+            name: 'profile',
+            component: () => import("@/views/UserProfile/UserProfile.vue")
+        },
+        {
+            path: '/create',
+            name: 'create',
+            component: () => import("@/views/UserCreate/UserCreate.vue")
+        },
+        {
+            path: '/edit/:id',
+            name: 'edit',
+            component: () => import("@/views/UserEdit/UserEdit.vue")
+        }
+    ]
 })
 
 export default router
