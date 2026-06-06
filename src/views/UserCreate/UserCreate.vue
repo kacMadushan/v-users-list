42fa90b0f1cd0c11fe5ed9284554ea46f010addf
kacMadushan <markuptutorials@gmail.com>
2026-06-07 00:57:28 +0530

    User add form data submit handler

--- src/views/UserCreate/UserCreate.vue
+++ src/views/UserCreate/UserCreate.vue
@@ -1,3 +1,22 @@
 <template>
-    <div>User create</div>
+    <div>
+        <h1>User create</h1>
+        <UserForm @add-new-user="addNewUser"/>
+    </div>
 </template>
+
+<script lang="js">
+import { mapActions } from "pinia"
+import { useUserStore } from "@/store/userStore";
+
+import UserForm from "@/components/UserForm.vue";
+
+export default {
+    components: {
+        UserForm
+    },
+    methods: {
+        ...mapActions(useUserStore, ['addNewUser'])
+    }
+}
+</script>
