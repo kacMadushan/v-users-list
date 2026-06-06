e61bc0cab24f5fb67703c72bdd30d9ac1fdb8502
kacMadushan <markuptutorials@gmail.com>
2026-06-06 21:32:51 +0530

    Create userlist userlistitem component

---
+++ src/views/Users/components/UserList.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+        <UserListItem v-for="user in users" :key="user.id" :user="user"/>
+    </ul>
+</template>
+
+<script lang="js">
+import UserListItem from "./UserListItem.vue";
+
+export default {
+    components: {
+        UserListItem
+    },
+    props: {
+        users: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
