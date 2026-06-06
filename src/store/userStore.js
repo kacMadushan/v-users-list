9e2e60184a85e6a58d7baa109552fe9b338b61cc
kacMadushan <markuptutorials@gmail.com>
2026-06-06 16:52:09 +0530

    Add users state local data

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,28 +1,39 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia"
 
-export const useUserStore = defineStore('user', {
+export const useUserStore = defineStore("user", {
     state: () => ({
         users: [
             {
                 id: "1",
-                name: "",
-                email: "",
-                mobile: "",
-                group: ""
+                name: "Emily Johnson",
+                email: "emily.johnson@x.dummyjson.com",
+                mobile: "+81 965-431-3024",
+                country: "United States",
+                group: "Office"
             },
             {
                 id: "2",
-                name: "",
-                email: "",
-                mobile: "",
-                group: ""
+                name: "Michael Williams",
+                email: "michael.williams@x.dummyjson.com",
+                mobile: "+49 258-627-6644",
+                country: "United States",
+                group: "Friend"
             },
             {
                 id: "3",
-                name: "",
-                email: "",
-                mobile: "",
-                group: ""
+                name: "Sophia Brown",
+                email: "sophia.brown@x.dummyjson.com",
+                mobile: "+81 210-652-2785",
+                country: "United States",
+                group: "Family"
+            },
+            {
+                id: "4",
+                name: "James Davis",
+                email: "james.davis@x.dummyjson.com",
+                mobile: "+49 614-958-9364",
+                country: "United States",
+                group: "Family"
             }
         ]
     }),
