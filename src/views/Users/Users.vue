00e24531112e9c7b00aad55da5eb2591eeaa445a
kacMadushan <markuptutorials@gmail.com>
2026-06-06 16:52:57 +0530

    Map users list

--- src/views/Users/Users.vue
+++ src/views/Users/Users.vue
@@ -1,3 +1,26 @@
 <template>
-    <div>Users page</div>
+    <div>
+        <h1>Users</h1>
+        <ul>
+            <li v-for="user in users" :key="user.id">
+                <div>
+                    <h5>{{ user.name }}</h5>
+                    <p>{{ user.email }}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
+
+<script lang="js">
+import { mapState } from "pinia"
+import { useUserStore} from "@/store/userStore"
+
+export default {
+    computed: {
+        ...mapState(useUserStore, ["users"]),
+        
+    }
+    
+}
+</script>
