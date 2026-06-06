58135ed609e79b9217271e9e87f83bebf3755e19
kacMadushan <markuptutorials@gmail.com>
2026-06-06 16:51:12 +0530

    Add double quotes import  vue-router lib

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router"
 import Users from "@/views/Users/Users.vue"
 
 const router = createRouter({
